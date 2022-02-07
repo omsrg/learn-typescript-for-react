fd059f4bfcfd4a12425be06ad4654fe8549bff68
omsrg <siregar.om30@gmail.com>
2022-02-07 19:00:10 +0700

    react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useEffect, useReducer, useRef } from "react";
 import "./App.css";
 
 const Heading = ({ title }: { title: string }) => {
@@ -9,31 +9,75 @@ const Box: React.FC = ({ children }) => {
 	return <div>{children}</div>;
 };
 
-const List: React.FC<{ items: string[]; onClick?: (item: string) => void }> = ({
-	items,
-	onClick,
-}) => {
-	return (
-		<ul>
-			{items.map((item, idx) => (
-				<li key={idx} onClick={() => onClick?.(item)}>
-					{item}
-				</li>
-			))}
-		</ul>
-	);
-};
+interface Payload {
+	text: string;
+}
+
+interface Todo {
+	id: number;
+	done: boolean;
+	text: string;
+}
+
+type ActionType = { type: "ADD"; text: string } | { type: "REMOVE"; id: number };
 
 function App() {
-	const onListClick = useCallback((item: string) => {
-		alert(item);
+	const [payload, setPayload] = useState<Payload | null>(null);
+
+	useEffect(() => {
+		fetch("/data.json")
+			.then((res) => res.json())
+			.then((data) => {
+				setPayload(data);
+			});
+	}, []);
+
+	const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
+		switch (action.type) {
+			case "ADD":
+				return [
+					...state,
+					{
+						id: state.length,
+						text: action.text,
+						done: false,
+					},
+				];
+			case "REMOVE":
+				return state.filter(({ id }) => id !== action.id);
+			default:
+				throw new Error();
+		}
+	}, []);
+
+	const newTodoRef = useRef<HTMLInputElement>(null);
+
+	const onAddTodo = useCallback(() => {
+		if (newTodoRef.current) {
+			dispatch({
+				type: "ADD",
+				text: newTodoRef.current.value,
+			});
+			newTodoRef.current.value = "";
+		}
 	}, []);
 
 	return (
 		<div className="App">
-			<Heading title="Introduction" />
-			<Box>Hello from the other side</Box>
-			<List items={["one", "two", "three"]} onClick={onListClick} />
+			<Box>{JSON.stringify(payload)}</Box>
+
+			<Heading title="Todos" />
+			{todos.map((todo) => (
+				<div key={todo.id}>
+					{todo.text}
+
+					<button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>Remove</button>
+				</div>
+			))}
+			<div>
+				<input type="text" ref={newTodoRef} />
+				<button onClick={onAddTodo}>Add Todo</button>
+			</div>
 		</div>
 	);
 }
