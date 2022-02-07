5983b7d267f5322cce4253e4600e2e6435a28cb7
omsrg <siregar.om30@gmail.com>
2022-02-07 22:13:22 +0700

    custom hooks

---
+++ src/04-custom-hooks/customHooks.tsx
@@ -0,0 +1,34 @@
+import React, { useRef, useCallback } from "react";
+import { useTodos } from "../useTodos";
+
+function EpsFour() {
+	const { todos, addTodo, removeTodo } = useTodos([{ id: 0, text: "Hey there", done: false }]);
+
+	const newTodoRef = useRef<HTMLInputElement>(null);
+
+	const onAddTodo = useCallback(() => {
+		if (newTodoRef.current) {
+			addTodo(newTodoRef.current.value);
+			newTodoRef.current.value = "";
+		}
+	}, [addTodo]);
+
+	return (
+		<div className="App">
+			<h1 style={{ marginBottom: "0.5rem", marginTop: "2rem" }}>Todos with Custom Hooks</h1>
+			{todos.map((todo) => (
+				<div key={todo.id}>
+					{todo.text}
+
+					<button onClick={() => removeTodo(todo.id)}>Remove</button>
+				</div>
+			))}
+			<div>
+				<input type="text" ref={newTodoRef} />
+				<button onClick={onAddTodo}>Add Todo</button>
+			</div>
+		</div>
+	);
+}
+
+export default EpsFour;
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import EpsOne from "./01-properties/01-props";
 import EpsTwo from "./02-hooks/02-hooks";
 import EpsThree from "./03-advanced-properties/03-props";
+import EpsFour from "./04-custom-hooks/customHooks";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<EpsOne />
 			<EpsTwo />
 			<EpsThree />
+			<EpsFour />
 		</div>
 	);
 }
